db8b8f862750e93c3ec08bc9bd4a2ac51c034cc1
Carlo Elvisi <carloelvisi01@outlook.it>
2024-11-19 15:47:04 +0100

    array creato

--- app.js
+++ app.js
@@ -9,3 +9,38 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
+
+const posts = [
+  {
+    titolo: "Ciambellone",
+    contenuto: "ciambellone",
+    immagine: "publicciambellone.jpeg",
+    tags: ["Ciambellone", "ciambella", "grande"],
+  },
+  {
+    titolo: "Cracker Barbabietola",
+    contenuto: "Cracker Barbabietola",
+    immagine: "publiccracker_barbabietola.jpeg",
+    tags: ["cracker", "barbabietola"],
+  },
+  {
+    titolo: "Pane Fritto Dolce",
+    contenuto: "Pane Fritto Dolce",
+    immagine: "publicpane_fritto_dolce.jpeg",
+    tags: ["pane", "fritto", "dolce"],
+  },
+  {
+    titolo: "Pasta Barbabietola",
+    contenuto: "Pasta Barbabietola",
+    immagine: "publicpasta_barbabietola.jpeg",
+    tags: ["pasta", "barbabietola"],
+  },
+  {
+    titolo: "Torta Paesana",
+    contenuto: "Torta Paesana",
+    immagine: "public\torta_paesana.jpeg",
+    tags: ["torta", "paesana"],
+  },
+];
+
+console.log(posts);
